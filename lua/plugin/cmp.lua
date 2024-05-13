4ccd0afab6ea6cc8545fa64b146b57bc7107cf69
hanpiJoker <spacejiahao@gmail.com>
2024-05-13 10:46:21 +0800

    update cmp config

--- lua/plugin/cmp.lua
+++ lua/plugin/cmp.lua
@@ -1,12 +1,7 @@
 -- Set up nvim-cmp.
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
 local luasnip = require('luasnip')
-
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
 
 cmp.setup({
 	snippet = {
@@ -111,3 +106,7 @@ cmp.setup.cmdline(':', {
 	}
 })
 
+cmp.event:on(
+	'confirm_done',
+	cmp_autopairs.on_confirm_done()
+)
