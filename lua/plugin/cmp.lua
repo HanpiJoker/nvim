e5cb67bd6063b2fbc3158f9971848c7617f9285e
hanpiJoker <spacejiahao@gmail.com>
2024-05-15 14:45:54 +0800

    update cmp config

--- lua/plugin/cmp.lua
+++ lua/plugin/cmp.lua
@@ -115,7 +115,6 @@ require("cmp_git").setup()
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ '/', '?' }, {
-	mapping = cmp.mapping.preset.cmdline(),
 	sources = {
 		{ name = 'buffer' }
 	}
@@ -123,7 +122,6 @@ cmp.setup.cmdline({ '/', '?' }, {
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-	mapping = cmp.mapping.preset.cmdline(),
 	sources = cmp.config.sources(
 		{
 			{ name = 'path' }
