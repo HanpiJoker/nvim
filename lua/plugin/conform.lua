9914a633af34c64dbc5810fbb84ecd11189aedda
hanpiJoker <spacejiahao@gmail.com>
2024-05-16 20:19:54 +0800

    use conform and nvim-lint plugin

--- lua/plugin/conform.lua
+++ lua/plugin/conform.lua
@@ -1,35 +1,22 @@
 -- Format synchronously on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		-- Disable autoformat on certain filetypes
-		local ignore_filetypes = { "sql", "java" }
-		if vim.tbl_contains(ignore_filetypes, vim.bo[args.buf].filetype) then
-			return
-		end
-		-- Disable with a global or buffer-local variable
-		if vim.g.disable_autoformat or vim.b[args.buf].disable_autoformat then
-			return
-		end
-		-- Disable autoformat for files in a certain path
-		local bufname = vim.api.nvim_buf_get_name(args.buf)
-		if bufname:match("/node_modules/") then
-			return
-		end
-		require("conform").format({ timeout_ms = 500, lsp_fallback = true, bufnr = args.buf })
-	end,
-})
 
 require("conform").setup({
-	format_on_save = function(bufnr)
+	format_after_save = function(bufnr)
+		local ignore_filetypes = { "sql", "java", "c", "cpp" }
+
+		if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+			return
+		end
+
 		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
 			return
 		end
+
 		-- ...additional logic...
-		return { timeout_ms = 500, lsp_fallback = true }
+		return { timeout_ms = 500, lsp_fallback = true, async = true }
 	end,
 	formatters_by_ft = {
 		c = { "clang-format" },
 		lua = { "stylua" },
-	}
+	},
 })
