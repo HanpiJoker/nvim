a3c3589b492e304a6e33acf4ad868cb2e8a42376
hanpiJoker <spacejiahao@gmail.com>
2024-05-29 16:52:09 +0800

    add neovide support

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -21,4 +21,8 @@ local modules = {
 for key, value in pairs(modules) do
 	package.loaded[value] = nil
 	require(value)
+end
+
+if vim.g.neovide then
+	require("neovide")
 end
---
+++ lua/neovide.lua
@@ -0,0 +1,2 @@
+vim.o.guifont = "SauceCodePro Nerd Font Mono:h16"
+vim.g.neovide_transparency = 0.8
