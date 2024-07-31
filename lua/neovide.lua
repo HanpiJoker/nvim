1f583d31e4713f56967a0e84b4acc9ea2c5421e1
yujiahao <yujiahao@cambricon.com>
2024-07-31 09:51:16 +0800

    update config

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,2 +1,6 @@
-vim.o.guifont = "SauceCodePro Nerd Font Mono:h16"
-vim.g.neovide_transparency = 0.8
+vim.o.guifont = "SauceCodePro Nerd Font Mono:h14"
+vim.g.neovide_transparency = 1
+vim.g.neovide_cursor_vfx_mode = "sonicboom"
+vim.g.neovide_fullscreen = true
+vim.g.neovide_cursor_animate_command_line = true
+vim.g.neovide_cursor_animate_in_insert_mode = true
