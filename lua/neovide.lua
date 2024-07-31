11b756944304f5659099c882abb9e7fe85da9bf0
HanpiJoker <spacejiahao.gmail.com>
2024-07-31 18:00:36 +0800

    upate telescope undo config

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,6 +1,11 @@
 vim.o.guifont = "SauceCodePro Nerd Font Mono:h14"
 vim.g.neovide_transparency = 1
 vim.g.neovide_cursor_vfx_mode = "sonicboom"
-vim.g.neovide_fullscreen = true
+-- vim.g.neovide_fullscreen = true
 vim.g.neovide_cursor_animate_command_line = true
 vim.g.neovide_cursor_animate_in_insert_mode = true
+vim.g.neovide_floating_shadow = true
+vim.g.neovide_floating_z_height = 10
+vim.g.neovide_light_angle_degrees = 45
+vim.g.neovide_light_radius = 5
+vim.g.neovide_show_border = true
