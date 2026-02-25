0b1be2b4bc6ffcfdc5d7e2037a3f87358f0dfaa7
HanpiJoker <spacejiahao@gmail.com>
2026-02-25 20:03:07 +0800

    refactor config file

--- lua/plugin/nvim-lint.lua
+++ lua/plugin/nvim-lint.lua
@@ -29,13 +29,9 @@ require("lint").linters_by_ft = {
 	zsh = { "shellcheck" },
 	c = { "cppcheck" },
 }
-require("lint").try_lint()
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 	callback = function()
 		require("lint").try_lint()
-		if vim.fn.filereadable(".vale.ini") > 0 then
-			require("lint").try_lint({ "vale" })
-		end
 	end,
 })
