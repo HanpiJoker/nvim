d5fe5dea8012042f430658efacb4fe83cd5aa15a
HanpiJoker <spacejiahao@gmail.com>
2026-06-23 19:20:55 +0800

    support image.nvim which will be enabled in kitty

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1157,6 +1157,14 @@ require("lazy").setup({
 		end,
 	},
 	{
+		"3rd/image.nvim",
+		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+		opts = {
+			processor = "magick_cli",
+			backend = "kitty",
+		},
+	},
+	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		opts = {
